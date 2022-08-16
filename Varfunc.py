ca9ed9c4aef9b2a2db5b4b6d29142518faa74c0c
Arian Syah Putra <71995420+ariansp@users.noreply.github.com>
2022-08-17 02:31:23 +0700

    Its about variabel

---
+++ Varfunc.py
@@ -0,0 +1,11 @@
+name = "Koyan"
+print(name) #Print var name
+print(type(name)) #Print the type of name
+print(type(name) == str) #checking the var type same as " "
+print(isinstance(name,str)) #same as above
+###############################
+#changing the variabel type
+number = "20"
+print(isinstance(number,int))
+number1 = int("20")
+print(isinstance(number1,int))
