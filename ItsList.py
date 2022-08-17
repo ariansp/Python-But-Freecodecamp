faa56de69ba4ebdfc7bd2e162c86feaa9103d183
Arian Syah Putra <71995420+ariansp@users.noreply.github.com>
2022-08-17 21:48:53 +0700

    Its about List
    some func here (Remove, add, pop)

---
+++ ItsList.py
@@ -0,0 +1,15 @@
+dogs = ["Koyan", 1, "Rian", True, "Ray1", "Ray"]
+print(dogs)
+print(dogs[2])
+
+print("""ADD
+###########""")
+dogs += ["I am New"]
+print(dogs)
+print(dogs.pop())
+
+print("""REMOVE
+###########""")
+dogs.remove("Rian")
+print(dogs.pop())
+print(dogs)
