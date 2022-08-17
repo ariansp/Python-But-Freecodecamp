539ddf14a8ea7bd6cd4bb4cf50bf8b86506fd65c
Arian Syah Putra <71995420+ariansp@users.noreply.github.com>
2022-08-17 21:10:53 +0700

    Datatype
    open it bro, it will be usefull

---
+++ Datatypes.py
@@ -0,0 +1,17 @@
+num1 = 1+2j+3j
+num2 = complex(2,3)
+print(num1)
+print(num2)
+#Lets see the different real and imagine num
+print(num1.real, num1.imag)
+print(num2.real, num2.imag)
+print("""
+
+""")
+#Another func
+#Make absolute num
+print(abs(-12.5))
+#make a round number
+print(round(5.23))
+print(round(5.83))
+print(round(5.23, 1))
