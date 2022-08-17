10aefaea1c341228974181bf60343a55fa587fcb
Arian Syah Putra <71995420+ariansp@users.noreply.github.com>
2022-08-17 20:43:29 +0700

    Its about string. But, it think its only half

---
+++ halfofstring.py
@@ -0,0 +1,23 @@
+#String
+name = "Koyan"
+name1 = "Koyan\""
+name2 = "Koy\\an" #Backslash
+print("Just ordinary string")
+#String with multiple line
+print("""Line 1
+Line 2
+Line 3""")
+#String to upper/lower
+print("Upper me".upper())
+print("LOWER ME!!!".lower())
+#Lenght of string
+print(len(name))
+#Print var with double quote
+print(name1)
+#Print backslash on var
+print(name2)
+#Print index on char
+print(name[1])
+print(name[-1]) #from the last char
+#Print var with a range, last range will be -1
+print(name[1:3])
