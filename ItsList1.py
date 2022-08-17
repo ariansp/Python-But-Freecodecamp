b4f9d655bcd34d0e555ee40b7d61ed533d078aba
Arian Syah Putra <71995420+ariansp@users.noreply.github.com>
2022-08-17 22:02:12 +0700

    List
    how about sorting list?

---
+++ ItsList1.py
@@ -0,0 +1,9 @@
+items = ["Book", 1, "Camera", "Car"]
+items.insert(2,"Bike")
+print(items)
+items[1:1] = ["Bike1", "Bike2"]
+print(items)
+#SortingList
+number = [1, 4, 3, 2, 7, 5, 2]
+number.sort()
+print(number)
